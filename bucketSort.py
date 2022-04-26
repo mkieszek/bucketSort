9698181506601821d4e2336013fefc9dcd648972
Marcin Kieszek <marcin.kieszek@viait.pl>
2022-04-26 20:34:49 +0200

    Update bucketSort.py

--- bucketSort.py
+++ bucketSort.py
@@ -1,67 +1,57 @@
-    #1. p = weź liczbę osób na dzień
-    # 2. d = weź liczbę dni
-
-    # dla każdego dnia
-    # - podaj liczbę uczników
-    #     dla każdego ucznia w dniu podaj id i średnią
-
-    # złącz pary w jedną tablicę
-
-    # znajdź max średnią
-    #size = max / len(tab)
-    # size = ustal ilość koszyków max średnia / ilość wszystkich uczniów
-
-    # stwórz tablice koszyków
-
-    # dla każdej pary (id, średnia):
-    #     index = int(para[1]/size)
-    #     if index != ilość uczniów:
-    #         koszyki[index].append(para)
-    #     else
-    #         koszyki[ilość uczniów - 1].append(para)
-        
-
-    # dla każdego koszyka wykonać sortowanie po średniej
-
-    # złączyć koszyki w jedną listę
-
-    # wydrukować dla każdego dnia p uczniów
+from operator import itemgetter
 
 
-def bucketSort(tab, k):
+def bucketSort(tab):
     #znajdź max średnią
-    top = 0
+    min = 100
     for i in range(len(tab)):
-        if(float(top) < float(tab[i][1])):
-            top = tab[i][1]
-    size = (max(tab) - min(tab)) / k
-    koszyki = ""
-    for x in range(1):
-        index = (tab[x] - min(tab)) / size
-        if index != len(tab):
-            koszyki = tab
+        if(min >= tab[i][1]):
+            min = tab[i][1]
+
+    size = int(len(tab) / min)
+
+    koszyki = []
+
+    for i in range(size):
+        koszyki.append([])
+
+    for element in tab:
+        index = int((element[1])/ size)
+
+        if index != size:
+            koszyki[index].append(element)
         else:
-            koszyki[len(tab) - 1] = arr
-    koszyki = tab
+            koszyki[size - 1].append(element)
+    
     posortowane = []
-    for j in range(len(koszyki)):
-        koszyki[j][1].sort()
-        for x in tab[j]:
-            posortowane.append(x)
+    
+    for koszyk in koszyki:
+        posortowanyKoszyk = sorted(koszyk, key=itemgetter(1))
+        for element in posortowanyKoszyk:
+            posortowane.append(element)
+
     return posortowane
 
 a = input().split()
 p = int(a[0])
 d = int(a[1])
 posort = []
+
+arr = []
+    
 for i in range(d):
-    arr = []
     n = int(input())
     for j in range(n):
         b = input().split()
         id = b[0]
         srednia = b[1]
         arr.append([int(id), float(srednia)])
-    arr = bucketSort(arr, 3)
-    for z in range(p):
-        print(arr[z][0])
+
+posort = bucketSort(arr)
+
+i = 0
+for dzien in range(d):
+    for uczen in range(p):
+        print(posort[i], end= ' ')
+        i = i + 1
+    print()
